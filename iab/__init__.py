29946429c3096ae6b2cab06ff25441a7ccdcfd88
Greg Caporaso <gregcaporaso@gmail.com>
2015-06-18 11:22:17 -0700

    MAINT: added __version__ variable

---
+++ iab/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# -----------------------------------------------------------------------------
+# This work is licensed under the Creative Commons
+# Attribution-NonCommercial-ShareAlike 4.0 International License. To view a
+# copy of this license, visit
+# http://creativecommons.org/licenses/by-nc-sa/4.0/.
+# -----------------------------------------------------------------------------
+
+__version__ = '0.1.0-dev'
