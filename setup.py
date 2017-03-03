0bd1b72c3e4dce815f5a6375f245b04fdab7e49c
Greg Caporaso <gregcaporaso@gmail.com>
2017-03-02 17:22:01 -0700

    MAINT: update to dev version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 # http://creativecommons.org/licenses/by-nc-sa/4.0/.
 # -----------------------------------------------------------------------------
 
-__version__ = '0.1.2'
+__version__ = '0.1.2-dev'
 
 from setuptools import find_packages, setup
 import sys
