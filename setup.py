c595238d66140bd492b4eaaf078bdbfd9b7b0385
Greg Caporaso <gregcaporaso@gmail.com>
2017-03-06 15:31:08 -0700

    REL: update version for 0.1.3 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 # http://creativecommons.org/licenses/by-nc-sa/4.0/.
 # -----------------------------------------------------------------------------
 
-__version__ = '0.1.3.dev0'
+__version__ = '0.1.3'
 
 from setuptools import find_packages, setup
 import sys
